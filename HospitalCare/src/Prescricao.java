60688c943eb6f69bda96271dad6dd3dcfcb941a9
FLAVQ <FLAVQ@DESKTOP-GKTUCML>
2018-03-23 15:40:04 +0000

    Prescricao updated

--- HospitalCare/src/Prescricao.java
+++ HospitalCare/src/Prescricao.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Prescricao {
-
+	private Doente doente;
+	private Medico medico;
+	private Medicamento medicamento;
+	
+	private Date data;
+	
+	private ArrayList<HorarioToma> horario;
+	
+	public Doente getDoente() {
+		return doente;
+	}
+	public void setDoente(Doente doente) {
+		this.doente = doente;
+	}
+	public Medico getMedico() {
+		return medico;
+	}
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	public Medicamento getMedicamento() {
+		return medicamento;
+	}
+	public void setMedicamento(Medicamento medicamento) {
+		this.medicamento = medicamento;
+	}
+	
+	public Prescricao() {
+		horario = new ArrayList<HorarioToma>();
+	}
+	
+	public Prescricao(Doente doente, Medico medico, Medicamento medicamento, Date data) {
+		this.doente = doente;
+		this.medico = medico;
+		this.medicamento = medicamento;
+		this.data = data;
+	}
+	
 }
